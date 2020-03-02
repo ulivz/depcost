ad6792c259119750e9f3820e35f777d01734e8cd
ULIVZ <472590061@qq.com>
2020-03-03 01:46:00 +0800

    fix: issue of parsePackageName & improve test.

---
+++ __tests__/parsePackageName.test.js
@@ -0,0 +1,47 @@
+const parse = require('../lib/parsePackageName')
+
+describe('parsePackageName', function () {
+  it('unscoped package', () => {
+    expect(parse('foo')).toEqual({
+      name: 'foo',
+      version: undefined
+    })
+
+    expect(parse('foo@')).toEqual({
+      name: 'foo',
+      version: undefined
+    })
+
+    expect(parse('foo@latest')).toEqual({
+      name: 'foo',
+      version: 'latest'
+    })
+
+    expect(parse('foo@1.0.0')).toEqual({
+      name: 'foo',
+      version: '1.0.0'
+    })
+  })
+
+  it('scoped package', () => {
+    expect(parse('@foo/bar')).toEqual({
+      name: '@foo/bar',
+      version: undefined
+    })
+
+    expect(parse('@foo/bar@')).toEqual({
+      name: '@foo/bar',
+      version: undefined
+    })
+
+    expect(parse('@foo/bar@latest')).toEqual({
+      name: '@foo/bar',
+      version: 'latest'
+    })
+
+    expect(parse('@foo/bar@1.0.0')).toEqual({
+      name: '@foo/bar',
+      version: '1.0.0'
+    })
+  })
+});
