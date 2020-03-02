7a83b4ba45a856880342e4eb3691483cd1122384
ULIVZ <472590061@qq.com>
2020-03-03 00:15:58 +0800

    refactor: api.

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
 
 const cac = require('cac')
-const ret = require('../lib')
+const { DepCost, DepCostEvents } = require('../lib')
 
 const cli = cac()
 
 cli
   .command('[...pkgs]', 'My Default Command')
   .option('-t, --track', 'Whether to keep temp directory.')
+  .option('-l, --log-level <logLevel>', 'Logger level.')
   .action((pkgs, flags) => {
-    ret({
+    const program = new DepCost({
       ...flags,
-      pkgs
-    }).catch(error => {
+      pkgs,
+    })
+
+    program.on(DepCostEvents.message, result => {
+      console.log(`${result.pkg}\t\t${result.size}\t\t${result.requireTime}`)
+    })
+
+    program.runAndEmit().catch(error => {
       process.exitCode = 1
       console.log(require('chalk').red(error.stack))
     })
